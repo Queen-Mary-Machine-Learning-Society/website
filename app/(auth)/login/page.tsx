87b72131b2f0e30cdfd30efdc95477b0259b16a1
Sam Yu <samchenyu@gmail.com>
2025-03-11 22:06:50 +0000

    Added Google and Credential Authentication

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,50 @@
 "use client";
+
 import GoogleButton from "react-google-button";
 import { signIn } from "next-auth/react";
+import { useState } from "react";
+
 
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+  
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault(); 
+        setError(""); // Reset in case of previous error
+        const result = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        });
+        if (result?.error) {
+            setError("Invalid email or password"); 
+        } else {
+            // Todo: redirect to authenticated page
+            window.location.href = "/signup"; 
+        }
+    };
+
+
     return (
         <div>
             <h1>Login Page</h1>
+            <form onSubmit = {handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}required/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password"  id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+                <button type="submit">Submit</button>
+                {error && <p>{error}</p>} 
+            </form>
+            
+
             <GoogleButton onClick={ () => signIn('google')} ></GoogleButton>
         </div>
     )
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,12 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import { SessionStrategy } from 'next-auth';
 //import bcrypt from 'bcrypt';
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET || !process.env.NEXTAUTH_SECRET) {
     throw new Error("Missing Google OAuth environment variables.");
 }
-
-
 
 export const authOptions = {
     providers: [
@@ -15,23 +14,12 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-}
-
-
-/*
-
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: clientId,
-            clientSecret: clientSecret
-        }),
 
         CredentialsProvider ({
             name: 'Credentials',
             credentials: {
+                id: { label: 'ID', type: 'text' },
+                name: { label: 'Name', type: 'text' },
                 email: { label: 'Email', type: 'email' },
                 password: { label: 'Password', type: 'password' },
             },
@@ -41,32 +29,39 @@ export const authOptions = {
                 if(!credentials.email || !credentials.password) return null;
 
                 // Fetch from databae
-                if (true || (user && bcrypt.compareSync(password, user.passwordHash)) ) { // Todo: Implement password hash
-                    return { id: user.id, name: user.name, email: user.email }
-                } else {
-                    throw new Error('Invalid credentials'); // Todo: Custom error handling
-                }
+                //if ((user && bcrypt.compareSync(password, user.passwordHash)) ) { // Todo: Implement password hash
+                //    return { id: user.id, name: user.name, email: user.email }
+                //} else {
+                //    throw new Error('Invalid credentials'); // Todo: Custom error handling
+                //}
+
+                // Mock user
+                const user = { id: "123", name: "John Doe", email: "johndoe@gmail.com"};
+                return user;
             }
         })
     ],
     secret: process.env.NEXTAUTH_SECRET,
     session: {
-        strategy: 'jwt',
+        strategy: 'jwt' as SessionStrategy,
         maxAge: 30 * 24 * 60 * 60, // 30 days
     },
     jwt: {
-        //secret: process.env.JWT_SECRET,
+        secret: process.env.JWT_SECRET,
     },
-    callbacks: {},
+    callbacks: {
+        async redirect({url, baseUrl}: { url: string; baseUrl: string }){
+            return baseUrl + '/signup'; // Todo: redirect to authenticated page
+        },
+    },
     pages: {
-        signIn: '/auth/signin',
+        signIn: '/auth/signup',
         signOut: '/auth/signout',
         error: '/auth/error',
-        verifyRequest: '/auth/verify-request',
-        newUser: null,
+        newUser: undefined,
     },
 }
-*/
+
 
 const handler = NextAuth(authOptions);
 // Export either as HTTP or POST request
