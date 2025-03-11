b8b715f04aa49e63e54955bbc74160c8de1c7d02
Sam Yu <samchenyu@gmail.com>
2025-03-11 18:43:13 +0000

    Google Sign in added

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import GoogleButton from "react-google-button";
+import { signIn } from "next-auth/react";
+
 export default function Login() {
     return (
         <div>
             <h1>Login Page</h1>
+            <GoogleButton onClick={ () => signIn('google')} ></GoogleButton>
         </div>
     )
 }
