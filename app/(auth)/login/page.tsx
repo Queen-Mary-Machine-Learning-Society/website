6b7d732a0d52aed946fb147c65dc0e0c5aa196f8
Ahnaf <itseahossain@gmail.com>
2025-03-12 12:08:20 +0000

    testpr

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
     return (
         <div>
-            <h1 className="text-3xl font-bold text-center">Login Page</h1>
+            <h1 className="text-3xl font-bold text-center underline">Login Page</h1>
         </div>
     )
 }
