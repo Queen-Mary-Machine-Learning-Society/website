f30262315b907b402b74eac5b3115c538413d48f
Jisu Park <gam2zer01@gmail.com>
2025-04-06 20:39:34 +0100

    Event Form Submission

---
+++ app/forms/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import WelcomeSection from '@/components/WelcomeSection';
+import EventsSection from '@/components/EventsSection';
+import AimsSection from '@/components/AimsSection';
+import JoinUsSection from '@/components/JoinUsSection';
+import Form from "@/components/Form";
+
+export default function Home() {
+  return (
+    <div className="bg-[#14213D]">
+      <Form />
+    </div>
+  );
+}
