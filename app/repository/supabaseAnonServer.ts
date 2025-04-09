bb84d6797ffc30e8753dad53f2ae21b058b3d18f
Jisu Park <gam2zer01@gmail.com>
2025-04-09 23:55:47 +0100

    Deleted duplicated supabase script for anon server

--- app/repository/supabaseAnonServer.ts
+++ app/repository/supabaseAnonServer.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!;
-const serviceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabaseAnon = createClient(supabaseUrl, serviceRoleKey);
+export const supabaseAnon = createClient(supabaseUrl, supabaseAnonKey);
 
 /*
 *   Supabase has two tables:
