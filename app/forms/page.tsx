7810ac38fc01fcd99a626d825bb1e5559938902d
Roman Gurung <romang2103@gmail.com>
2025-04-10 15:37:15 +0100

    Padding added to top of form

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -5,9 +5,9 @@ import AimsSection from '@/components/AimsSection';
 import JoinUsSection from '@/components/JoinUsSection';
 import Form from "@/components/Form";
 
-export default function Home() {
+export default function CreateEventPage() {
   return (
-    <div className="bg-[#14213D]">
+    <div className="bg-[#14213D] px-6 py-21">
       <Form />
     </div>
   );
